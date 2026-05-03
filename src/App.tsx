6c52704f38836392509fd32e051aaa6b1cf99f98
= rasmusmaatta <= maatta.rasmus@gmail.com>
2026-05-03 21:57:52 +0300

    basename muutettu

--- src/App.tsx
+++ src/App.tsx
@@ -67,7 +67,7 @@ function App() {
           </Toolbar>
         </AppBar>
         <Routes>
-          <Route path="/personaltrainerapp" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/customers" element={<CustomerList />} />
           <Route path="/trainings" element={<TrainingList />} />
           <Route path="/calendar" element={<Calendar />} />
