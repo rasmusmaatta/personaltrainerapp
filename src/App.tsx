74aedd71ccc09e62bc7f38a714b2bddb2653f5ce
= rasmusmaatta <= maatta.rasmus@gmail.com>
2026-05-06 18:36:30 +0300

    ..

--- src/App.tsx
+++ src/App.tsx
@@ -67,7 +67,7 @@ function App() {
           </Toolbar>
         </AppBar>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/personaltrainerapp" element={<Home />} />
           <Route path="/customers" element={<CustomerList />} />
           <Route path="/trainings" element={<TrainingList />} />
           <Route path="/calendar" element={<Calendar />} />
