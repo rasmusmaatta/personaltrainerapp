9bb2d49171038e2cb63c1fa6ed9ff5d1520e4446
= rasmusmaatta <= maatta.rasmus@gmail.com>
2026-05-06 23:21:41 +0300

    pages ongelman selvittely

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/personaltrainerapp/">
+    <BrowserRouter basename="/personaltrainerapp">
       <App />
     </BrowserRouter>
   </StrictMode>,
